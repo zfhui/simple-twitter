074fa4836b223bf159e7109cefef622c6e61a2be
Zhuo-Fei Hui <fei@posteo.de>
2018-02-18 17:31:15 +0100

    Change setting the search query to before_action

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,6 +1,13 @@
 class TwitterController < ApplicationController
+  before_action :search_query
+
   def search
-    q = params[:q]
-    @tweets = q.present? ? TWITTER.search(q, result_type: 'recent').take(5) : []
+    @tweets = @q.present? ? TWITTER.search(@q, result_type: 'recent') : []
+  end
+
+  private
+
+  def search_query
+    @q ||= params[:q] || ''
   end
 end
